207e49a23a464546c589c44d8e0ccfd7504535c9
MaksymMikolajczyk <zielony4444@gmail.com>
2019-09-30 18:31:27 +0200

    Admin panel start
    Added basic layout and pages

--- client/src/App.js
+++ client/src/App.js
@@ -7,31 +7,45 @@ import Footer from "./components/layout/Footer";
 import Root from "./components/Root";
 import Shop from "./components/screens/Shop";
 import SignIn from "./components/auth/SignIn";
-import Admin from "./components/Admin";
+import Admin from "./components/Admin/Admin";
 import Mission from "./components/screens/events/Mission";
 
 import withAuth from "./hoc/withAuth";
 
 class App extends React.Component {
   state = {
-    fields: []
+    fields: [],
+    isAdmin: false
   };
+
+
+  //FOR PRESENTATION ONLY
+  componentDidMount() {
+    const isAdmin = localStorage.getItem("isAdmin") ? true : false;
+    this.setState({ isAdmin });
+  }
 
   render() {
     return (
       <BrowserRouter>
         <StylesProvider injectFirst>
-          <div className="App">
-            <Navbar />
-            <Switch>
-              <Route exact path="/" component={withAuth(Root)} />
-              <Route exact path="/shop" component={withAuth(Shop)} />
-              <Route exact path="/mission" component={withAuth(Mission)} />
-              <Route exact path="/signin" component={SignIn} />
-              <Route exact path="/console" component={Admin} />
-            </Switch>
-            <Footer />
-          </div>
+          {this.state.isAdmin ? (
+            <div className="App">
+              <Route exact path="/" component={withAuth(Admin)} />
+            </div>
+          ) : (
+            <div className="App">
+              <Navbar />
+              <Switch>
+                <Route exact path="/" component={withAuth(Root)} />
+                <Route exact path="/shop" component={withAuth(Shop)} />
+                <Route exact path="/mission" component={withAuth(Mission)} />
+                <Route exact path="/signin" component={SignIn} />
+                <Route exact path="/admin" component={Admin} />
+              </Switch>
+              <Footer />
+            </div>
+          )}
         </StylesProvider>
       </BrowserRouter>
     );
---
+++ client/src/components/Admin/Pages/AdminQR.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const AdminQR = () => {
+  return (
+    <div>
+        <p>Kamera do czytania kodu</p>
+        <p>Koszyk po wczytaniu kodu</p>
+    </div>
+  )
+}
+
+export default AdminQR
