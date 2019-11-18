802fba2ba06c9a2953be249be8a498dc617c8f8d
ashad912 <krzysztof.los912@gmail.com>
2019-11-18 19:38:23 +0100

    rally routes and futher cron work

--- server/src/routes/rally.js
+++ server/src/routes/rally.js
@@ -1,6 +1,5 @@
 import express from 'express'
 import cron from 'node-cron'
-import axios from 'axios'
 import { Rally} from '../models/rally';
 import { auth } from '../middleware/auth';
 import { Item } from '../models/item';
@@ -8,20 +7,118 @@ import { asyncForEach } from '../utils/methods'
 
 const router = new express.Router
 
+var rallyTask
+
+const updateRallyQueue = async () => {
+    try {
+        const startRally = await Rally.find({status: 'active'}).sort({"activationDate": -1 }).limit(1)
+        const finishRally = await Rally.find({status: 'active'}).sort({"expiryDate": -1 }).limit(1)
+
+        if(!startRally || !finishRally){
+            console.log('There is no rally for update criteria!')
+            return
+        }
+
+        if(startRally._id !== finishRally._id){
+            console.log('przypau')
+        }
+        //...
+        // designate schedule time
+
+        //finishRally
+        rallyTask = cron.schedule("* * * * *", async () => {
+            console.log(`this message logs every minute`);
+            try{
+                await finishRally()
+                
+            }catch(e) {
+                console.log(e.message)
+            }
+            
+            
+        });
+
+    }catch (e) {
+        return
+    }
+}
 
 
-
-router.post('/createRally', auth, async (req, res) =>{
+router.post('/rally', auth, async (req, res) =>{
 
     const rally = new Rally(req.body)
 
     try {
         await rally.save()
+        await updateRallyQueue()
         res.status(201).send(rally)
     } catch (e) {
         res.status(400).send(e)
     }
 })
+
+
+router.put('/rally', auth, async (req, res) =>{
+
+    try {
+        let rally = await Rally.findById(req.body._id)
+
+        rally = req.body
+
+        //??delete rally._id
+        
+        await rally.save()
+        await updateRallyQueue()
+        res.status(200).send(rally)
+    } catch (e) {
+        res.status(400).send(e)
+    }
+})
+
+router.patch("/rally", auth, async (req, res, next) => {
+    const updates = Object.keys(req.body);
+    // const allowedUpdates = ["name", "password"];
+  
+    // const isValidOperation = updates.every(update => {
+    //   return allowedUpdates.includes(update);
+    // });
+  
+    // if (!isValidOperation) {
+    //   return res.status(400).send({ error: "Invalid update!" });
+    // }
+  
+    try {
+      const rally = await Rally.findById(req.body._id)
+  
+      updates.forEach(update => {
+        rally[update] = req.body[update]; //rally[update] -> rally.name, rally.password itd.
+      });
+  
+      await rally.save();
+  
+      res.send(rally);
+    } catch (e) {
+      res.status(400).send(e);
+    }
+  });
+
+router.delete('/rally', auth, async (req, res) =>{
+
+    try {
+        const rally = await Rally.findOneAndDelete({_id: req.body._id})
+
+        if(!rally){
+            res.status(404).send()
+        }
+        await updateRallyQueue()
+        res.send()
+    } catch (e) {
+        res.status(500).send(e.message)
+    }
+})
+
+
+
 //TO-DO: FINISH!!!!
 async function addAwards(user, awardsLevels){
     let items = []
@@ -44,33 +141,47 @@ async function addAwards(user, awardsLevels){
     return items  
 }
 
-router.post('/finishRally', auth, async (req, res) => {
-    const rally = await Rally.findById(req.body._id).populate({
-        path: 'awardsLevels.awardsLevel.awards'
-    }).populate({
-        path: 'users.profile'
-    }).execPopulate()
+const finishRally = async () => {
+    try{
+        const rally = await Rally.findById(req.body._id).populate({
+            path: 'awardsLevels.awardsLevel.awards'
+        }).populate({
+            path: 'users.profile'
+        }).execPopulate()
 
-    const users = rally.users
+        const users = rally.users
 
-    await asyncForEach(users, async (rallyUser) => {
-        const user = await User.findById(rallyUser.profile._id).populate({
-            path: 'activeRally'
+        await asyncForEach(users, async (rallyUser) => {
+            
+                const user = await User.findById(rallyUser.profile._id).populate({
+                    path: 'activeRally'
+                })
+                if(user.activeRally && rallyUser.experience > 0){
+                    const items = await addAwards(rallyUser, rally.awardsLevels)
+                    user.bag = [...user.bag, items]
+                }
+                await user.save() 
+            
+            
         })
-        if(user.activeRally && rallyUser.experience > 0){
-            const items = await addAwards(rallyUser, rally.awardsLevels)
-            user.bag = [...user.bag, items]
-        } 
-        
-    })
+
+        rally.status = 'archive'
+        await rally.save()
+        rallyTask.destroy()
+        return
+
+    }catch(e){
+        return e
+    }
 
 
-})
+
+}
 
 
 
 router.get('/triggerCron', auth, async(req, res) => {
-    cron.schedule("* * * * *", async () => {
+    rallyTask = cron.schedule("* * * * *", async () => {
         console.log(`this message logs every minute`);
         try{
             const res = getCron()
@@ -87,6 +198,7 @@ router.get('/triggerCron', auth, async(req, res) => {
 
 const getCron = () => {
     console.log('hello from getCron')
+    rallyTask.destroy()
     return Date.now()
 }
 
